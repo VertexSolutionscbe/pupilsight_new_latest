8e7e002612d6243244660840bcd6c3a7dcf88865
bikash0389 <bikash@thoughtnet.in>
2020-10-15 19:19:07 +0530

    update by bikash

--- cms/application_page.php
+++ cms/application_page.php
@@ -469,7 +469,6 @@ $app_links = array();
             var pid = $("#pid").val();
             var fid = $("#fid").val();
             var clid = $("#class option:selected").val();
-            //alert(clid);
             if (val != '') {
                 var type = 'insertcampaigndetails';
                 setTimeout(function() {
