7b0d67cdd471063554b49292b7c4e34afaed3911
rakesh <rakesh@thoughtnet.in>
2020-10-17 16:42:57 +0530

    update

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
 include('C:/xampp/htdocs/pupilsight/config.php');
 
-$conn = new mysqli($databaseServer,$databaseUsername,$databasePassword,$databaseName);
+$conn = new mysqli($databaseServer, $databaseUsername, $databasePassword, $databaseName);
 
 // Check connection
-if ($conn -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+if ($conn->connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli->connect_error;
   exit();
 }
- 
