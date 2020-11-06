3d18abf35b01b27e4480d3902f62cc9f05626334
rakesh <rakesh@thoughtnet.in>
2020-11-06 10:24:46 +0530

    update

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ Pupilsight, Flexible & Open School System
 $databaseServer = '127.0.0.1';
 $databaseUsername = 'root';
 $databasePassword = 'xyz';
-$databaseName = 'pupilsight';
+$databaseName = 'pd_demo'; //pupilsight
 
 // if (session_status() == PHP_SESSION_NONE) {
 //     session_start();
