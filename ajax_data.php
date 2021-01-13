962560633002b1531654c1fb9834284ed608fa65
bikash0389 <bikash@thoughtnet.in>
2021-01-13 11:55:41 +0530

    update by bikash

--- ajax_data.php
+++ ajax_data.php
@@ -703,9 +703,9 @@ if ($type == 'getClass') {
     $uid = $_SESSION[$guid]['pupilsightPersonID'];
     $pid = $val;
     if($roleId == '2'){
-        $sql = 'SELECT a.*, b.name FROM assign_class_teacher_section AS a LEFT JOIN pupilsightYearGroup AS b ON a.pupilsightYearGroupID = b.pupilsightYearGroupID WHERE a.pupilsightPersonID = "'.$uid.'" AND a.pupilsightProgramID = "' . $pid . '" AND b.pupilsightSchoolYearID = "'.$pupilsightSchoolYearID.'" GROUP BY a.pupilsightYearGroupID';
+        $sql = 'SELECT a.*, b.name FROM assign_class_teacher_section AS a LEFT JOIN pupilsightYearGroup AS b ON a.pupilsightYearGroupID = b.pupilsightYearGroupID WHERE a.pupilsightPersonID = "'.$uid.'" AND a.pupilsightProgramID = "' . $pid . '"  GROUP BY a.pupilsightYearGroupID';
     } else {
-        $sql = 'SELECT a.*, b.name FROM pupilsightProgramClassSectionMapping AS a LEFT JOIN pupilsightYearGroup AS b ON a.pupilsightYearGroupID = b.pupilsightYearGroupID WHERE a.pupilsightProgramID = "' . $pid . '" AND b.pupilsightSchoolYearID = "'.$pupilsightSchoolYearID.'" GROUP BY a.pupilsightYearGroupID';
+        $sql = 'SELECT a.*, b.name FROM pupilsightProgramClassSectionMapping AS a LEFT JOIN pupilsightYearGroup AS b ON a.pupilsightYearGroupID = b.pupilsightYearGroupID WHERE a.pupilsightProgramID = "' . $pid . '"  GROUP BY a.pupilsightYearGroupID';
     }
     
     $result = $connection2->query($sql);
--- modules/Academics/test_create_with_section.php
+++ modules/Academics/test_create_with_section.php
@@ -126,12 +126,12 @@ if (isActionAccessible($guid, $connection2, '/modules/Academics/test_create_with
      $col->addLabel('pupilsightYearGroupID', __('Class'));
      $col->addSelect('pupilsightYearGroupID')->setId('pupilsightYearGroupID_check')->required()->placeholder('Select Class');
  
-     //$col = $row->addColumn()->setClass('newdes');
-    // $col->addLabel('pupilsightRollGroupID', __('Section'));
-     //$col->addSelect('pupilsightRollGroupID', $pupilsightSchoolYearID)->required()->placeholder('Select Section');
      $col = $row->addColumn()->setClass('newdes');
-     $col->addLabel('pupilsightRollGroupID', __('Section'));
-     $col->addContent('<div id="pupilsightRollGroupID_check" class="section_div w-full txtfield"></div> ');
+    $col->addLabel('pupilsightRollGroupID', __('Section'));
+     $col->addSelect('pupilsightRollGroupID[]')->setId('pupilsightRollGroupID_check')->required()->placeholder('Select Section');
+    //  $col = $row->addColumn()->setClass('newdes');
+    //  $col->addLabel('pupilsightRollGroupID', __('Section'));
+    //  $col->addContent('<div id="pupilsightRollGroupID_check" class="section_div w-full txtfield"></div> ');
      
      $col = $row->addColumn()->setClass('newdes');
      $col->addLabel('test_master_id', __('Test'));
@@ -213,6 +213,24 @@ if (isActionAccessible($guid, $connection2, '/modules/Academics/test_create_with
 }
 
 </style>
+
+<script>
+    $(document).on('change', '#pupilsightYearGroupID_check', function () {
+        var id = $(this).val();
+        var pid = $('#pupilsightProgramID_check').val();
+        var type = 'getSection';
+        $.ajax({
+            url: 'ajax_data.php',
+            type: 'post',
+            data: { val: id, type: type, pid: pid },
+            async: true,
+            success: function (response) {
+                $("#pupilsightRollGroupID_check").html('');
+                $("#pupilsightRollGroupID_check").html(response);
+            }
+        });
+    });
+</script>
 
 
 
