137b31b11043682dfbef53d9affda0df8716ad8b
rakesh <rakesh@thoughtnet.in>
2021-03-03 19:49:51 +0530

    update

--- config.php
+++ config.php
@@ -16,9 +16,8 @@ $databaseUsername = 'root';
 //$databaseName = 'bips_erp'; //pd_demo
 //$databaseName = 'ups'; //pd_demo
 //live
-$databasePassword = ''; //xyz
-
-$databaseName = 'christacademy'; //pd_demo
+$databasePassword = 'xyz'; //xyz
+$databaseName = 'pd_demo'; //pd_demo
 
 //if (session_status() == PHP_SESSION_NONE) {
 //    session_start();
