3c177b349723eadfae089b62f387934767e0550a
Rakesh Kumar <it.rakesh@gmail.com>
2021-07-02 10:21:38 +0530

    update

--- archive_fee_recipt_map.php
+++ archive_fee_recipt_map.php
@@ -3,7 +3,7 @@ include 'pupilsight.php';
 $session = $container->get('session');
 //before start check table is created or not
 die();
-$loc = $_SERVER['DOCUMENT_ROOT']."/public/archive/fee_receipt"; //file location
+$loc = $_SERVER['DOCUMENT_ROOT'] . "/public/archive/fee_receipt"; //file location
 $htmlFiles = glob("$loc/*.{html,htm}", GLOB_BRACE); //only html files
 //print_r($htmlFiles); //for debug files check here
 //die();
@@ -11,51 +11,53 @@ $len = count($htmlFiles);
 $i = 0;
 $result = array();
 $sq = "";
-while($i<$len){
-    if($sq){
-        $sq .=",";
+while ($i < $len) {
+    if ($sq) {
+        $sq .= ",";
     }
     $res = getFileStudentDetails($htmlFiles[$i]);
-    if($res){
-        $sq .="('".$res["student_id"]."','".$res["student_name"]."','".$res["st_class"]."','".$res["st_date"]."','".$res["receipt_no"]."','".$res["file_html"]."')";
+    if ($res) {
+        $sq .= "('" . $res["student_id"] . "','" . $res["student_name"] . "','" . $res["st_class"] . "','" . $res["st_date"] . "','" . $res["receipt_no"] . "','" . $res["file_html"] . "')";
     }
     $i++;
 }
 //print_r($result);
-$sql = 'insert into archive_fee_receipt_html (student_id, student_name, st_class, st_date, receipt_no, file_html) VALUES '.$sq;
+$sql = 'insert into archive_fee_receipt_html (student_id, student_name, st_class, st_date, receipt_no, file_html) VALUES ' . $sq;
 //get 
 echo $sql;
-try{
+try {
     $connection2->query($sql);
-}catch(Exception $ex){
+} catch (Exception $ex) {
     echo $ex->getMessage();
 }
 
-function getFileStudentDetails($fileName){
-    
+function getFileStudentDetails($fileName)
+{
+
     $lines = file($fileName, FILE_IGNORE_NEW_LINES);
     //print_r($lines);
-    $tag = array("Receipt No","Date","Student Name","Class","Student Id");
-    $tagkey = array("receipt_no","st_date","student_name","st_class","student_id");
+    $tag = array("Receipt No", "Date", "Student's Name", "Class/Section", "Student ID");
+    //$tag = array("Receipt No","Date","Student Name","Class","Student Id");
+    $tagkey = array("receipt_no", "st_date", "student_name", "st_class", "student_id");
     $i = 0;
     $ilen = count($lines);
     $result = array();
     $result["file_html"] = basename($fileName);
-    while($i<$ilen){
+    while ($i < $ilen) {
 
         $len = count($tag);
         $j = 0;
-        while($j<$len){
+        while ($j < $len) {
             $pos = strpos($lines[$i], $tag[$j]);
-            if($pos !== false) {
-                $dt = strip_tags($lines[$i]);//data 
-                $dtv = str_replace($tag[$j],"",$dt); //data value
+            if ($pos !== false) {
+                $dt = strip_tags($lines[$i]); //data 
+                $dtv = str_replace($tag[$j], "", $dt); //data value
 
                 $dtvf = preg_replace('/[:\/]/', '', $dtv); //final value
 
                 $result[$tagkey[$j]] = trim($dtvf);
-                array_splice($tag, $j,1);
-                array_splice($tagkey, $j,1);
+                array_splice($tag, $j, 1);
+                array_splice($tagkey, $j, 1);
                 break;
             }
             $j++;
@@ -64,4 +66,3 @@ function getFileStudentDetails($fileName){
     }
     return $result;
 }
-?>
